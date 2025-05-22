18ac428371969429ad2fd358c859ff125b640d64
JeanCarlos <jmatochepascual@gmail.com>
2025-05-22 09:51:57 -0500

    feat: :recycle: Refactor test form

--- test/components/Form.test.tsx
+++ test/components/Form.test.tsx
@@ -5,75 +5,44 @@ import { vi } from 'vitest';
 
 describe('Test in <Form/>', () => {
   test('Debe mostrar el input con el valor por defecto', () => {
-    render(<Form
-      inputValue="Naruto"
-      setInputValue={() => { }}
-      searchGif={async () => { }}
-      setIsLoading={() => { }}
-    />);
+    render(<Form searchGif={async () => { }} setIsLoading={() => { }} />);
 
     const input = screen.getByPlaceholderText('Buscar Gif') as HTMLInputElement;
-    expect(input.value).toBe('Naruto');
+    expect(input.value).toBe('');
   });
 
-  test('Debe llamar a setInputValue cuando se escribe', () => {
-    const setInputValue = vi.fn();
+  test('Debe cambiar el valor del input cuando se escribe', () => {
+    render(<Form searchGif={async () => { }} setIsLoading={() => { }} />);
+    const input = screen.getByPlaceholderText('Buscar Gif') as HTMLInputElement;
 
-    render(
-      <Form
-        inputValue=""
-        setInputValue={setInputValue}
-        searchGif={async () => { }}
-        setIsLoading={() => { }}
-      />
-    );
+    fireEvent.input(input, { target: { value: 'Goku' } });
 
-    const input = screen.getByPlaceholderText('Buscar Gif');
-    fireEvent.change(input, { target: { value: 'Goku' } });
-
-    expect(setInputValue).toHaveBeenCalledWith('Goku');
+    expect(input.value).toBe('Goku');
   });
 
   test('No debe llamar searchGif si inputValue está vacío', () => {
     const searchGif = vi.fn();
-    const setInputValue = vi.fn();
-    const setIsLoading = vi.fn();
-
-    render(
-      <Form
-        inputValue=""
-        setInputValue={setInputValue}
-        searchGif={searchGif}
-        setIsLoading={setIsLoading}
-      />
-    );
-
+    render(<Form searchGif={searchGif} setIsLoading={() => { }} />);
     const form = screen.getByRole('form');
+
     fireEvent.submit(form);
 
     expect(searchGif).not.toHaveBeenCalled();
-    expect(setIsLoading).not.toHaveBeenCalled();
   });
 
-  test('Debe llamar funciones al enviar si inputValue tiene texto', async () => {
-    const setInputValue = vi.fn();
+  test('Debe llamar las funciones al si el inputValue tiene texto', async () => {
     const setIsLoading = vi.fn();
     const searchGif = vi.fn().mockResolvedValue(undefined);
-
-    render(
-      <Form
-        inputValue="One Piece"
-        setInputValue={setInputValue}
-        searchGif={searchGif}
-        setIsLoading={setIsLoading}
-      />
-    );
-
+    render(<Form searchGif={searchGif} setIsLoading={setIsLoading} />);
+    const inputValue = 'Goku';
+    const input = screen.getByPlaceholderText('Buscar Gif') as HTMLInputElement;
     const form = screen.getByRole('form');
+
+    fireEvent.input(input, { target: { value: inputValue } });
     fireEvent.submit(form);
 
     expect(setIsLoading).toHaveBeenCalledWith(true);
-    expect(searchGif).toHaveBeenCalledWith('One Piece');
-    expect(setInputValue).toHaveBeenCalledWith('');
+    expect(searchGif).toHaveBeenCalledWith(inputValue);
+    expect(input.value).toBe('');
   });
 });
--- test/components/Gif.test.tsx
+++ test/components/Gif.test.tsx
@@ -7,13 +7,14 @@ import { Gift } from '../../src/models/Gift';
 
 describe('Test <Gif/>', () => {
   const gif: Gift = {
-    id: 'asdasdasd',
-    title: 'Titulo del componente',
+    id: 'sdf645',
+    title: 'Goku',
     image: 'https://goku.com/goku.jpg'
   };
 
   test('Debe mostrar el url y el alt indicado', () => {
     render(<GIf gif={gif} />);
+
     // expect(screen.getByRole('img').src).toBe(gif.image);
     const { src, alt } = screen.getByRole('img') as HTMLImageElement;
     expect(src).toBe(gif.image);
