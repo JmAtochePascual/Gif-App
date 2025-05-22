9fed7d130b79c849d6ff391b51d380ba74cfe829
JeanCarlos <jmatochepascual@gmail.com>
2025-05-22 15:04:12 -0500

    feat: :test_tube: Refactor test customHook

--- test/hooks/useGif.test.ts
+++ test/hooks/useGif.test.ts
@@ -1,36 +1,17 @@
-import { renderHook, act } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import useGif from '../../src/hooks/useGif';
-import * as getGiftsService from '../../src/services/getGifs';
-import { describe, test, vi, expect, beforeEach } from 'vitest';
+import { describe, test, expect } from 'vitest';
 
 describe('useGif Hook', () => {
-  beforeEach(() => {
-    vi.clearAllMocks();
-  });
-
   test('Debe tener estado inicial correcto', () => {
     const { result } = renderHook(() => useGif());
 
-    expect(result.current.inputValue).toBe('');
     expect(result.current.sections).toEqual([]);
     expect(result.current.isLoading).toBe(false);
   });
 
   test('Debe actualizar el estado al hacer una búsqueda', async () => {
-    const mockGif = {
-      id: '123',
-      title: 'Goku',
-      images: { downsized_medium: { url: 'http://test.gif' } },
-    };
-
-    vi.spyOn(getGiftsService, 'getGifts').mockResolvedValue([mockGif]);
-
     const { result } = renderHook(() => useGif());
-
-    act(() => {
-      result.current.setInputValue('Goku');
-      result.current.setIsLoading(true);
-    });
 
     await act(async () => {
       await result.current.searchGif('Goku');
@@ -38,7 +19,6 @@ describe('useGif Hook', () => {
 
     expect(result.current.sections.length).toBe(1);
     expect(result.current.sections[0].title).toBe('Goku');
-    expect(result.current.sections[0].gifs[0].id).toBe('123');
     expect(result.current.isLoading).toBe(false);
   });
 });
